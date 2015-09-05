6dc23edf1bcbb896d6c8f8e5b444e7b657db29bb
Alan <u3055235@gmail.com>
2015-09-05 14:52:48 +0800

    edit 1. init/GlobalService.java 	6: databaseName=bellyworry

--- src/init/GlobalService.java
+++ src/init/GlobalService.java
@@ -1,8 +1,9 @@
 package init;
 
 public class GlobalService {
-	public static final String host = "127.0.0.1";
-	public static final String URL = "jdbc:sqlserver://" + GlobalService.host + ":1433;databaseName=JSPDB";
+	public static final String host = "192.168.149.130";
+//	public static final String host = "127.0.0.1";
+	public static final String URL = "jdbc:sqlserver://" + GlobalService.host + ":1433;databaseName=bellyworry";
 	public static final String USERNAME = "sa";
 	public static final String PASSWORD = "sa123456";
 	public static final String JNDI_DB_NAME = "java:comp/env/jdbc/bellyworryDataSQLver";
