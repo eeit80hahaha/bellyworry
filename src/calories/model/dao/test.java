a63f607e1b44ebc4fbfca1dffe326e25e6aa731c
K <c02360236@gmail.com>
2015-09-07 20:14:17 +0800

    1.update "Connection conn = DriverManager.getConnection(GlobalService.URL, GlobalService.USERNAME, GlobalService.PASSWORD);" 2.Integrate test.file

--- src/calories/model/dao/test.java
+++ src/calories/model/dao/test.java
@@ -1,5 +1,7 @@
 package calories.model.dao;
 
+import calories.model.ExerciseCalVO;
+
 public class test {
 
 	public static void main(String[] args) {
@@ -49,6 +51,33 @@ public class test {
 
 		// =========================================================
 
+		// *****MenuDaoJdbc 測試檔!*****
+//		MenuDaoJdbc temp=new MenuDaoJdbc();
+//		System.out.println(temp.selectByPrimaryKey(100015));
+//		System.out.println(temp.getAll());
+//		MenuVO temp1=new MenuVO();	
+//		temp1.setMenuNo(100019);
+//		temp1.setName("類");
+//		System.out.println(temp.insert(temp1));
+//		System.out.println(temp.update(temp1));
+//		System.out.println(temp.delete(100019));
+		
+		// =========================================================
+		
+		// *****ExerciseCalDaoJdbc 測試檔!*****
+//		ExerciseCalDaoJdbc test=new ExerciseCalDaoJdbc();
+//		System.out.println(test.selectByPrimaryKey(100001));
+//		System.out.println(test.getAll());
+//		ExerciseCalVO test1=new ExerciseCalVO();		
+//		test1.setName("千球4");
+//		test1.setCalHour( 50.7F);//why 要型別轉換?
+//		test1.setExerciseNo(100008);
+//		System.out.println(test.insert(test1));
+//		System.out.println(test.update(test1));
+//		System.out.println(test.delete(100008));
+		
+		// =========================================================
+		
 	}
 
 }
---
+++ src/health/model/dao/test.java
@@ -0,0 +1,38 @@
+package health.model.dao;
+import calories.model.ExerciseCalVO;
+import calories.model.dao.ExerciseCalDaoJdbc;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		// *****HealthDiaryDaoJdbc 測試檔!*****	
+//		
+//		HealthDiaryDaoJdbc result=new HealthDiaryDaoJdbc();
+//		System.out.println(result.selectByPrimaryKey(1000002));
+//		System.out.println(result.getAll());
+//		HealthDiaryVO result1=new HealthDiaryVO();
+//		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd");
+//		result1.setMemberNo(1000017);
+//		try {
+//			result1.setDate(sFormat.parse("2003-03-21"));
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		result1.setHeight(182.5f);//why 要型別轉換?
+//		result1.setWeight(83.2f);//why 要型別轉換?
+//		result1.setWaistline(100.5f);//why 要型別轉換?
+//		result1.setTitle("我是測試");
+//		result1.setContent("我是測試我是");
+//		result1.setShare("1");
+//		result1.setNo(1000009);
+//		System.out.println(result.insert(result1));
+//		result.update(result1);
+//		System.out.println(result.delete(1000009));
+		
+		// =========================================================
+	}
+
+}
