c79e758d2536fb0539a5d8787230e80ec15ee74e
K <c02360236@gmail.com>
2015-09-14 13:15:29 +0800

    修改ExerciseCalDaoHbm、MenuDaoHbm 、HealthDiraryHbm的update方法

--- src/calories/model/dao/test.java
+++ src/calories/model/dao/test.java
@@ -1,32 +1,31 @@
 package calories.model.dao;
 
-import calories.model.FoodCalDAO;
-import calories.model.FoodCalVO;
+import calories.model.ExerciseCalVO;
 
 public class test {
 
 	public static void main(String[] args) {
 
 		// *****FoodCalDAOjdbc 測試檔!*****
-		FoodCalVO vo = new FoodCalVO();
-		FoodCalDAO dao = new FoodCalDAOjdbc();
+//		FoodCalVO vo = new FoodCalVO();
+//		FoodCalDAO dao = new FoodCalDAOjdbc();
 
 		// // selectByPrimaryKey
-		// vo = dao.selectByPrimaryKey(100009);
-		// System.out.println(vo);
+//		 vo = dao.selectByPrimaryKey(100001);
+//		 System.out.println(vo);
 
 		// // select_ALL
 		// System.out.println(dao.getAll());
 
 		// // INSERT
-		 vo.setName("水蜜桃");
-		 vo.setMenuNo(100007);
-		 vo.setCal(38);
-		 vo.setCount("顆");
-		 vo.setWeight(102);
-		 vo.setCookNo(100001);
-		 System.out.println(dao.insert(vo,null,0));
-		 System.out.println(dao.selectByPrimaryKey(100018));
+//		 vo.setName("水蜜桃");
+//		 vo.setMenuNo(100007);
+//		 vo.setCal(38);
+//		 vo.setCount("顆");
+//		 vo.setWeight(102);
+//		 vo.setCookNo(100001);
+//		 System.out.println(dao.insert(vo,null,0));
+//		 System.out.println(dao.selectByPrimaryKey(100018));
 
 		// vo.setName("美式咖啡");
 		// vo.setMenuNo(100018);
@@ -57,8 +56,8 @@ public class test {
 //		System.out.println(temp.selectByPrimaryKey(100001));
 //		System.out.println(temp.getAll());
 //		MenuVO temp1=new MenuVO();	
-//		temp1.setMenuNo(100004);
-//		temp1.setName("測試1");
+//		temp1.setMenuNo(100001);
+//		temp1.setName("測試22");
 //		System.out.println(temp.insert(temp1));
 //		System.out.println(temp.update(temp1));
 //		System.out.println(temp.delete(100019));
@@ -72,7 +71,7 @@ public class test {
 //		ExerciseCalVO test1=new ExerciseCalVO();		
 //		test1.setName("鉛球11");
 //		test1.setCalHour( 50.7F);//why 要型別轉換?
-//		test1.setExerciseNo(100007);
+//		test1.setExerciseNo(100001);
 //		System.out.println(test.insert(test1));
 //		System.out.println(test.update(test1));
 //		System.out.println(test.delete(100008));
